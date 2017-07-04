1002c65118af8af5487ef0c2af22fdbb53e3dae2
Baz Jay <Baz@Bazs-MacBook-Pro.local>
2017-07-04 22:11:26 +0100

    Updated folder structure and add excerise 7 webpack starter project

---
+++ exercise-7/vue-cli/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <p>Server Status: {{ status }}</p>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      status: 'Critical'
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
