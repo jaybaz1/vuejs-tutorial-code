d578c88dac7e26f24b140b88c05cebe653fbad80
Baz Jay <Baz@Bazs-MacBook-Pro.local>
2017-07-04 22:46:43 +0100

    Completed tutorial videos for exercise 7

---
+++ exercise-7/vue-cli/src/Home.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <app-server-status 
+            v-for="(server, index) in 5" 
+            v-bind:server="server"
+            v-bind:index="index"  
+            v-bind:key="server">
+        </app-server-status>
+    </div>
+</template>
+
+<script>
+    import ServerStatus from './ServerStatus.vue';
+
+    export default {
+        components:{
+            'app-server-status': ServerStatus
+        }
+    }
+</script>
