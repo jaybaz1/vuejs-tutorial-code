29886cf1f725b06e42749b82284436c0ae648524
Baz Jay <jaybaz1@gmail.com>
2017-07-09 22:11:00 +0100

    Working with components (Global)

--- practice-7-components/vue-cli/src/main.js
+++ practice-7-components/vue-cli/src/main.js
@@ -1,7 +1,16 @@
 import Vue from 'vue'
 import App from './App.vue'
+import Header from './Header.vue'
+import Footer from './Footer.vue'
+import Server from './Server.vue'
+import ServerDetails from './ServerDetails.vue'
 
-new Vue({
+Vue.component('app-header', Header);
+Vue.component('app-footer', Footer);
+Vue.component('app-server', Server)
+Vue.component('app-server-details', ServerDetails);
+
+new Vue({ 
   el: '#app',
   render: h => h(App)
 })
