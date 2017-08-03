84ecad61bdefdcfd048a71644a79ec2e44fab19c
Baz Jay <jaybaz1@gmail.com>
2017-08-03 23:00:55 +0100

    Exercise folder name updates

--- exercise-8-communicating-between-components/src/components/UserDetail.vue
+++ exercise-8-communicating-between-components/src/components/UserDetail.vue
@@ -2,10 +2,19 @@
     <div class="component">
         <h3>You may view the User Details here</h3>
         <p>Many Details</p>
+        <p>User name: {{ switchName() }}</p>
     </div>
 </template>
 
 <script>
+    export default { 
+        props: ['name'],
+        methods: {
+            switchName(){
+                return this.name.split("").reverse().join("");
+            }
+        }
+    }
 </script>
 
 <style scoped>
